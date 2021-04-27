ee01ab46bee1d347102ccf2968382aaffe104b97
vaov1999 <vaov1999@gmail.com>
2021-04-27 13:01:02 +0300

    Chart refactor

--- components/Chart.vue
+++ components/Chart.vue
@@ -13,51 +13,47 @@ export default {
   },
   mounted() {
     const { am4core, am4charts } = this.$amchart()
-    console.log('root amcharts', am4core, am4charts)
-    const chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart)
-    console.log('chart', chart)
+    const CHART = am4core.create(this.$refs.chartdiv, am4charts.XYChart)
 
-    chart.paddingRight = 20
+    CHART.paddingRight = 20
 
-    const data = []
-    let visits = 10
+    const DATA = []
+    let VISITS = 10
+
     for (let i = 1; i < 7; i++) {
-      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10)
-      data.push({
+      VISITS += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10)
+      DATA.push({
         date: new Date(
           new Date().getFullYear(),
           new Date().getMonth(),
           new Date().getDay()
         ),
         name: 'name' + i,
-        value: visits,
+        value: VISITS,
       })
     }
 
-    console.log('DATA', data)
-    console.log('new DATA', new Date())
+    CHART.data = DATA
 
-    chart.data = data
+    const DATE_AXIS = CHART.xAxes.push(new am4charts.DateAxis())
+    DATE_AXIS.renderer.grid.template.location = 0
 
-    const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
-    dateAxis.renderer.grid.template.location = 0
+    const VALUE_AXIS = CHART.yAxes.push(new am4charts.ValueAxis())
+    VALUE_AXIS.tooltip.disabled = true
+    VALUE_AXIS.renderer.minWidth = 35
 
-    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
-    valueAxis.tooltip.disabled = true
-    valueAxis.renderer.minWidth = 35
+    const SERIES = CHART.series.push(new am4charts.LineSeries())
+    SERIES.dataFields.dateX = 'date'
+    SERIES.dataFields.valueY = 'value'
 
-    const series = chart.series.push(new am4charts.LineSeries())
-    series.dataFields.dateX = 'date'
-    series.dataFields.valueY = 'value'
-
-    series.tooltipText = '{valueY.value}'
-    chart.cursor = new am4charts.XYCursor()
+    SERIES.tooltipText = '{valueY.value}'
+    CHART.cursor = new am4charts.XYCursor()
 
     const scrollbarX = new am4charts.XYChartScrollbar()
-    scrollbarX.series.push(series)
-    chart.scrollbarX = scrollbarX
+    scrollbarX.series.push(SERIES)
+    CHART.scrollbarX = scrollbarX
 
-    this.chart = chart
+    this.chart = CHART
   },
 }
 </script>
